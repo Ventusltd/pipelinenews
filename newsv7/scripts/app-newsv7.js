5a733a36a12c53c18a70a02ce8dd2c89c6687bde
Ventusltd <vikram.kumar@ventusltd.com>
2026-08-25 10:50:27 +0100

    perf(newsv7): defer intelligence until newspaper renders

--- newsv7/scripts/app-newsv7.js
+++ newsv7/scripts/app-newsv7.js
@@ -1,5 +1,4 @@
 import { initialiseGaugesV9_2, loadGaugeChartsNewsV7 } from "./plugins/gauges-v9-2.js";
-import { loadCumulativeIntelligenceNewsV7 } from "./plugins/intelligence-newsv7.js";
 import { bindNewspaperNewsV7, drawNewsNewsV7, loadNewsNewsV7 } from "./plugins/newspaper-newsv7.js";
 import {
   bindProjectControlsNewsV7,
@@ -8,32 +7,37 @@ import {
 } from "./plugins/projects-newsv7.js";
 import { startPlugins } from "./core/plugin-host.js";
 
-let intelligencePromise = null;
+function loadIntelligenceAfterNewspaper() {
+  setTimeout(async () => {
+    try {
+      const { loadCumulativeIntelligenceNewsV7 } = await import("./plugins/intelligence-newsv7.js");
+      await loadCumulativeIntelligenceNewsV7();
+      drawNewsNewsV7();
+      refreshProjectsNewsV7();
+    } catch (error) {
+      console.error("News V7 background intelligence unavailable", error);
+    }
+  }, 0);
+}
 
 startPlugins([
   { id: "gauges", start: initialiseGaugesV9_2 },
   {
-    id: "intelligence",
-    dependsOn: ["gauges"],
-    start() {
-      intelligencePromise = loadCumulativeIntelligenceNewsV7();
-    },
-  },
-  {
     id: "newspaper",
-    dependsOn: ["gauges", "intelligence"],
+    dependsOn: ["gauges"],
     start() {
       bindNewspaperNewsV7(refreshProjectsNewsV7);
       const newsPromise = loadNewsNewsV7();
-      Promise.allSettled([intelligencePromise, newsPromise]).then(() => {
+      Promise.resolve(newsPromise).then(() => {
         drawNewsNewsV7();
         refreshProjectsNewsV7();
+        loadIntelligenceAfterNewspaper();
       });
     },
   },
   {
     id: "projects",
-    dependsOn: ["gauges", "intelligence", "newspaper"],
+    dependsOn: ["gauges", "newspaper"],
     start() {
       bindProjectControlsNewsV7();
       loadProjectsNewsV7();
