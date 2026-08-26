de9615515b36cb438237fcbb2ca519799875e19f
Ventusltd <vikram.kumar@ventusltd.com>
2026-08-26 18:03:42 +0100

    202608261806 add modular V9.6.2 application bootstrap

---
+++ ui/202608261557-javascript/202608261806-app-v9-6-2.js
@@ -0,0 +1,28 @@
+import { initialiseGaugesV9_2 } from "./202608261725-gauges.js";
+import { bindNewspaperV9_6_2, loadNewsV9_6_2 } from "./202608261802-newspaper-v9-6-2.js";
+import {
+  bindProjectControlsV9_5_1,
+  loadProjectsV9_5_1,
+  refreshProjectsV9_5_1,
+} from "./202608261804-projects-v9-5-1.js";
+import { startPlugins } from "./202608261557-startplugins.js";
+
+startPlugins([
+  { id: "gauges", start: initialiseGaugesV9_2 },
+  {
+    id: "newspaper",
+    dependsOn: ["gauges"],
+    start() {
+      bindNewspaperV9_6_2(refreshProjectsV9_5_1);
+      loadNewsV9_6_2();
+    },
+  },
+  {
+    id: "projects",
+    dependsOn: ["gauges", "newspaper"],
+    start() {
+      bindProjectControlsV9_5_1();
+      loadProjectsV9_5_1();
+    },
+  },
+]);
